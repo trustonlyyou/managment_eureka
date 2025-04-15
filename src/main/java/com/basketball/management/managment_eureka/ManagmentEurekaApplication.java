36aa624fc0c528e3c7227096ccd24afadababbcf
trushonlyyou <trushonlyyou@tnctec.co.kr>
2025-04-16 00:45:01 +0900

    init project

---
+++ src/main/java/com/basketball/management/managment_eureka/ManagmentEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.basketball.management.managment_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagmentEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagmentEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basketball/management/managment_eureka/ManagmentEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basketball.management.managment_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagmentEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
